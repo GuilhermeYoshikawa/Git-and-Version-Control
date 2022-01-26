5cbf0bfb29b0cfed528447ead529f6b19d2a9f03
GuilhermeYoshikawa <61564238+GuilhermeYoshikawa@users.noreply.github.com>
2022-01-26 11:44:47 -0300

    Add files via upload

---
+++ first/Difference.java
@@ -0,0 +1,10 @@
+public class Difference {
+	public static void main(String args[]) {
+		int a = Integer.parseInt(JOptionPane.showInputDialog("Number1: "));
+		int b = Integer.parseInt(JOptionPane.showInputDialog("Number2: "));
+		int d = a - b;
+		
+		System.out.println("Difference Result: " + d);
+	}
+}
+
