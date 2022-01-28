c6b7cd111d98b971c66378fb93225afecacc2401
GuilhermeYoshikawa <61564238+GuilhermeYoshikawa@users.noreply.github.com>
2022-01-28 10:27:19 -0300

    Delete first directory

--- first/Add.java
+++
@@ -1,9 +0,0 @@
-public class Add {
-	public static void main(String args[]) {
-		int a = Integer.parseInt(JOptionPane.showInputDialog("Number1: "));
-		int b = Integer.parseInt(JOptionPane.showInputDialog("Number2: "));
-		int s = a + b;
-		
-		System.out.println("Sum Result: " + s);
-	}
-
